7fcc1d9e77b960a53e71e736dbcda57afccfcde9
артем рыбка <seledkin@live.com>
2019-04-23 22:09:35 +0300

    lesson5

---
+++ hw05_hard.py
@@ -0,0 +1,29 @@
+__author__ = 'Рыбка Артем Сергеевич'
+
+import math
+import random
+
+
+def zadanie1():
+    pass
+
+
+def zadanie2():
+    pass
+
+
+def zadanie3():
+    pass
+
+
+select = input("Введи номер задачи 1-3 \n")
+
+if select == "1":
+    zadanie1()
+elif select == "2":
+    zadanie2()
+elif select == "3":
+    zadanie3()
+
+else:
+    print("Неправильный ввод")
